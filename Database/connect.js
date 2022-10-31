8cb853b6ffc38601ed5f126e566e7401eb535c27
Tirth Govani <govanitirth@gmail.com>
2022-11-01 02:22:54 +0530

    Bug fixes

--- Database/connect.js
+++ Database/connect.js
@@ -1,6 +1,8 @@
 const mongoose  = require('mongoose')
+const dotenv = require('dotenv')
+dotenv.config()
 
-mongoose.connect("mongodb://localhost:27017/MINI_SAP").then(()=>{
+mongoose.connect(process.env.MONGO_URL).then(()=>{
     console.log("Database Connected....");
 }).catch((e)=>{
     console.log(e);
